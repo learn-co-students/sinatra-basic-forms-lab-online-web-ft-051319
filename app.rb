b1a6efc37013c1b7f9c323684d189620413d7ef2
Haley DeKeyser <hcd09@live.com>
2019-06-25 20:30:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -10,10 +10,9 @@ class App < Sinatra::Base
   end
   
   post '/puppy' do
-    @name = params["name"]
-    @breed = params["breed"]
-    @age = params["age"]
+    @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
     
     erb :display_puppy
   end
+  
 end
